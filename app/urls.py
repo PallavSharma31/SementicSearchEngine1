499bfdf0f0bc6b501afa1f18b8bd3d77a819f7a3
pallav31sharma <pallavofficial27@gmail.com>
2023-07-09 00:29:53 +0530

    login and signup functionality added

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import home
+from .views import *
 
 urlpatterns = [
-    path('', home,name="homepage"),
+    path('', SignupPage, name='signup'),
+    path('login/', LoginPage, name='login'),
+    path('home/', HomePage, name='home'),
+    path('logout/', LogoutPage, name='logout'),
+    path('search/',SearchPage,name='search')
 
 ]
--- app/views.py
+++ app/views.py
@@ -4,10 +4,69 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from sentence_transformers import SentenceTransformer,util
 import joblib
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 
-def home(request):
+
+def SignupPage(request):
+    if request.method == 'POST':
+        uname = request.POST.get('username')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('password1')
+        pass2 = request.POST.get('password2')
+
+        username_error_message = ""
+        email_error_message = ""
+        if User.objects.filter(username=uname).exists():
+            username_error_message= "Username is already taken"
+            # return render(request,'signup.html',{'username_error_messages':username_error_messages})
+        if User.objects.filter(email=email).exists():
+            email_error_message= "Email is already taken"
+            # return render(request, 'signup.html', {'email_error_messages': email_error_messages})
+
+        print(len(username_error_message))
+        print(len(email_error_message))
+        if len(username_error_message) > 0 or len(email_error_message) > 0:
+            return render(request, 'signup.html', {'username_error_message': username_error_message,
+                                                   'email_error_message': email_error_message})
+        else:
+            my_user = User.objects.create_user(uname, email, pass1)
+            my_user.save()
+            return redirect('search')
+
+    return render(request, 'signup.html')
+
+
+def LoginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        pass1 = request.POST.get('password')
+        user = authenticate(request, username=username, password=pass1)
+        if user is not None:
+            login(request, user)
+            return redirect('search')
+        else:
+            return render(request,'login.html',{'error_message':"Invalid Credential"})
+
+    return render(request, 'login.html')
+
+
+def LogoutPage(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
+def SearchPage(request):
+    return render(request,'search.html')
+
+
+@login_required(login_url='login')
+def HomePage(request):
     embeddings=joblib.load('C:/Users/pallav/PycharmProjects/Hackathon/AiSearch/app/embeddings')
     model=joblib.load('C:/Users/pallav/PycharmProjects/Hackathon/AiSearch/app/model')
     product_urls=joblib.load('C:/Users/pallav/PycharmProjects/Hackathon/AiSearch/app/product_urls')
@@ -62,7 +121,7 @@ def home(request):
     # new_df=new_df.iloc[10,:]
     list = new_df.to_dict('records')
     # new_list=new_df.head(10)
-    dic = {'list': list[:30]}
+    dic = {'list': list[:30],'request':request,'search':search}
 
     return render(request, 'home.html', dic)
 
